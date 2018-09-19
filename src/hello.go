8a545dd71e5d8a5fcd6edaf7ae22e6da8f2f47dd
smilekomiya <vetwith@gmail.com>
2018-09-19 11:39:36 +0900

    a tour of go 2章目終了

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
+
+var i, j int = 1, 2
 
 func main() {
-    fmt.Printf("Hello World\n")
+    var c, python, java = true, false, "no"
+    fmt.Println(i, j, c, python, java)
 }
