4e2bc9fbac60ec131d17f4600a0a5ca4dfbb6de0
smilekomiya <vetwith@gmail.com>
2018-09-30 22:58:00 +0900

    sliceに関して

--- src/slices-pointers.go
+++ src/slices-pointers.go
@@ -10,4 +10,12 @@ func main() {
         "Ringo",
     }
     fmt.Println(names)
+
+    a := names[0:2]
+    b := names[1:3]
+    fmt.Println(a, b)
+
+    b[0] = "XXX"
+    fmt.Println(a, b)
+    fmt.Println(names)
 }
